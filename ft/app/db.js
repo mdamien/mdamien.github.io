c3a24c11a858374337ccbc3c2fe1ac9a8556e6e5
Damien <damien@dam.io>
2015-03-30 09:03:57 +0200

    Demo of SQL in-memory loading and querying

--- ft/app/db.js
+++ ft/app/db.js
@@ -1,1 +1,48 @@
-//TODO: lovefield db
+DB = {}
+
+DB.escape = function (val) {
+  val = "'"+val.toString().replace(/[\0\n\r\b\t\\'"\x1a]/g, function (s) {
+    switch (s) {
+      case "\0":
+        return "\\0";
+      case "\n":
+        return "\\n";
+      case "\r":
+        return "\\r";
+      case "\b":
+        return "\\b";
+      case "\t":
+        return "\\t";
+      case "\x1a":
+        return "\\Z";
+      case "'":
+        return "''";
+      case '"':
+        return '""';
+      default:
+        return "\\" + s;
+    }
+  })+"'";
+
+  return val;
+}
+
+DB.load = function(data, next){
+  var db = new SQL.Database();
+
+  var sqlstr = 'DROP TABLE IF EXISTS "table";\n'
+  var cols = []
+  for (col in data[0]){
+      var type = 'char';
+      if(typeof data[0][col] === 'int'){
+          type = 'integer';
+      }
+      cols.push(DB.escape(col)+" "+DB.escape(type))
+  }
+  sqlstr += 'CREATE TABLE "table" ('+cols.join(',')+');\n'
+  data.forEach(function(line){
+    sqlstr += 'INSERT INTO "table" VALUES ('+_.values(line).map(DB.escape).join(',')+');\n'
+  })
+  db.run(sqlstr);
+  next(db)
+}
