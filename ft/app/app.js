5bdbf8cc477699437d9d159b07547ed006891c99
Damien <damien@dam.io>
2015-04-08 23:55:00 +0200

    start of dynamic csv loading

--- ft/app/app.js
+++ ft/app/app.js
@@ -1,4 +1,4 @@
-var App = React.createClass({
+var Tabs = React.createClass({
     getInitialState:function(){
         return {
             'current_tab':1,
@@ -62,3 +62,93 @@ var App = React.createClass({
             </div>);
     }
 })
+
+
+var App = React.createClass({
+    getInitialState: function(){
+        return {
+            loading: false,
+            data_url: this.props.data_url,
+            data: [],
+        }
+    },
+    componentDidMount: function(){
+        this.load();
+    },
+    start_load: function(){
+        this.setState({
+            data_url:this.refs.data_url.getDOMNode().value,
+            data:[],
+        },this.load);
+    },
+    load: function(){
+        this.setState({loading:true})
+        Papa.parse(this.state.data_url, {
+            download: true,
+            header: true,
+            dynamicTyping: true,
+            complete: function(results) {
+                this.setState({
+                    loading:false,
+                    data:results.data,
+                })
+            }.bind(this)
+        });
+    },
+    render: function(){
+        var message = "";
+        var content = "";
+        if(this.state.loading){
+            message = <strong>loading .csv</strong>;
+        }
+        else if(this.state.data.length > 0){
+            content = (<Tabs data={this.state.data} tabs={TABS} />);
+            message = (<span>Imported <strong>{this.state.data_url}</strong
+                > with <strong>{this.state.data.length}</strong> lines
+                </span>);
+        }
+        return (<div>
+            <input ref="data_url" defaultValue={this.state.data_url}/>
+                <button onClick={this.start_load}>load</button>  {message}
+            {content}
+        </div>);
+    },
+})
+
+var TABS = [
+    {
+        name:'teble',
+        type:'table',
+    },{
+        name:'chert',
+        type:'chart',
+        x:{
+            col:'timestamp',
+        },
+        y:{
+            col:'price_usd',
+        },
+        scatter: true,
+        filters:[
+            {
+                column:'drug_name',
+                value:"mdma",
+                type:"contains",
+            },
+            {
+                column:'price_usd',
+                value:"x < 1000",
+                type:"formula",
+            },
+        ],
+    },{
+        name:'a nother chert',
+        type:'chart',
+        x:{
+            col:'timestamp',
+        },
+        y:{
+            col:'product_rating',
+        },
+    }
+]
