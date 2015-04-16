5864f98bf20116195581dfd7212e41083d5b6f29
Damien <damien@dam.io>
2015-04-16 02:11:38 +0200

    add viz - login.corp.google.com - initial proto

---
+++ fun/viz/main.js
@@ -0,0 +1,6 @@
+
+React.render(<strong>App crashed, it loaded at least!</strong>, document.getElementById('content'))
+
+React.render( <App 
+    data_url="https://cdn.rawgit.com/mdamien/2b1a06c3a112e5ab5c05/raw/1a8ce5f0f7c1e2622159b0ed9cd89ad12fe6fc71/urls" />, document.getElementById('content'))
+
