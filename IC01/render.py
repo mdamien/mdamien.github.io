bab82e7141068aace34f59037a94b09cdeebb869
Damien <damien@dam.io>
2015-06-14 20:09:41 +0200

    add other summarizer methods

--- IC01/render.py
+++ IC01/render.py
@@ -1,37 +1,37 @@
 from flask import Flask, render_template
+from flask import Response
 app = Flask(__name__)
 
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.lsa import LsaSummarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
 LANGUAGE = "french"
 SENTENCES_COUNT = 20
 
-def summary(text):
+def summary(text, summarizer_class):
     parser = PlaintextParser.from_string(text, Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
 
-    summarizer = Summarizer(stemmer)
+    summarizer = summarizer_class(stemmer)
     summarizer.stop_words = get_stop_words(LANGUAGE)
 
     for sentence in summarizer(parser.document, SENTENCES_COUNT):
         print(sentence)
         yield sentence
 
-@app.route("/")
-def hello():
+def yield_me_some_courses(summarizer):
     import json
 
     ALL = json.load(open('all.json'))
 
-    courses = []
     for key in sorted(ALL.keys(), key=lambda x:int(x)):
         el = ALL[key]
-        courses.append((key, el))
         el['sentences'] = []
 
         try:
@@ -39,10 +39,33 @@ def hello():
             print("suggs",len(sugg))
             text = '\n\n'.join([s.get('content').get('description') for s in sugg])
             print("text",len(text))
-            el['sentences'] = list(summary(text))
+            el['sentences'] = list(summary(text, summarizer))
         except Exception as e:
             print(e)
-    return render_template("index.html", courses=courses)
+        yield (key, el)
+
+
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    rv.enable_buffering(5)
+    return rv
+
+@app.route("/")
+def hello():
+    return Response(stream_template('index.html', courses=yield_me_some_courses(summarizer=LsaSummarizer)))
+
+@app.route("/text")
+def textrank():
+    return Response(stream_template('index.html',
+            courses=yield_me_some_courses(summarizer=TextRankSummarizer)))
+
+
+@app.route("/lex")
+def lexrank():
+    return Response(stream_template('index.html', courses=yield_me_some_courses(summarizer=LexRankSummarizer)))
+
 
 @app.route("/all/")
 def all():
